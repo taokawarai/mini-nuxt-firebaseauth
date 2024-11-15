a28f373cacbd6b117fb50e9bdb59fa2c3e47b820
Tao KAWARAI <taotako@gmail.com>
2024-11-16 00:01:15 +0900

    Add comment

--- src/middleware/auth.ts
+++ src/middleware/auth.ts
@@ -1,6 +1,7 @@
 export default defineNuxtRouteMiddleware(async () => {
   const { user, initAuth } = useAuth()
   
+  // もしログインしていなかったら、トップページにリダイレクトする
   if (process.client) {
     await initAuth()
     if (!user.value) {
